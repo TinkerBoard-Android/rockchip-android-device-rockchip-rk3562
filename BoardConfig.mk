ba6015f366a523a8477f2aae3dddbc64fd2abf36
Binyuan Lan <lby@rock-chips.com>
2022-12-13 03:39:50 +0000

    Initial rk3562 board with Android 13
    Signed-off-by: Binyuan Lan <lby@rock-chips.com>
    Change-Id: Ie132c47006d67af87a03dc293210b1a54d1d9532

---
+++ BoardConfig.mk
@@ -0,0 +1,122 @@
+#
+# Copyright (c) 2020 Rockchip Electronics Co., Ltd
+#
+
+CURRENT_SDK_VERSION := rk3562_ANDROID13.0_MID_V1.0
+
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+PRODUCT_UBOOT_CONFIG ?= rk3562
+PRODUCT_KERNEL_ARCH ?= arm64
+PRODUCT_KERNEL_DTS ?= rk3562-evb1-lp4x-v10
+PRODUCT_KERNEL_CONFIG ?= rockchip_defconfig rk356x.config
+
+# BOARD_AVB_ENABLE := true
+# used for fstab_generator, sdmmc controller address
+PRODUCT_BOOT_DEVICE := ff870000.sdhci
+
+SF_PRIMARY_DISPLAY_ORIENTATION := 0
+
+# Disable emulator for "make dist" until there is a 64-bit qemu kernel
+BUILD_EMULATOR := false
+TARGET_BOARD_PLATFORM := rk3562
+TARGET_BOARD_PLATFORM_GPU := mali-G52
+TARGET_RK_GRALLOC_VERSION := 4
+BOARD_USE_DRM := true
+
+# RenderScript
+# OVERRIDE_RS_DRIVER := libnvRSDriver.so
+BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a53
+BOARD_OVERRIDE_RS_CPU_VARIANT_64 := cortex-a53
+# DISABLE_RS_64_BIT_DRIVER := true
+
+TARGET_USES_64_BIT_BCMDHD := true
+TARGET_USES_64_BIT_BINDER := true
+
+PRODUCT_KERNEL_VERSION := 5.10
+
+# HACK: Build apps as 64b for volantis_64_only
+ifneq (,$(filter ro.zygote=zygote64, $(PRODUCT_DEFAULT_PROPERTY_OVERRIDES)))
+TARGET_PREFER_32_BIT_APPS :=
+TARGET_SUPPORTS_64_BIT_APPS := true
+endif
+
+# Sensors
+BOARD_SENSOR_ST := true
+BOARD_SENSOR_MPU_VR := false
+BOARD_SENSOR_MPU_PAD := false
+
+BOARD_USES_GENERIC_INVENSENSE := false
+
+TARGET_BOARD_PLATFORM_PRODUCT ?= tablet
+
+ENABLE_CPUSETS := true
+
+# Enable Dex compile opt as default
+WITH_DEXPREOPT := true
+
+BOARD_NFC_SUPPORT := false
+BOARD_HAS_GPS := false
+
+BOARD_GRAVITY_SENSOR_SUPPORT := true
+BOARD_COMPASS_SENSOR_SUPPORT := false
+BOARD_GYROSCOPE_SENSOR_SUPPORT := false
+BOARD_PROXIMITY_SENSOR_SUPPORT := false
+BOARD_LIGHT_SENSOR_SUPPORT := false
+BOARD_PRESSURE_SENSOR_SUPPORT := false
+BOARD_TEMPERATURE_SENSOR_SUPPORT := false
+BOARD_USB_HOST_SUPPORT := true
+
+BOARD_USE_SPARSE_SYSTEM_IMAGE := true
+
+# Google Service and frp overlay
+BUILD_WITH_GOOGLE_MARKET := false
+BUILD_WITH_GOOGLE_MARKET_ALL := false
+BUILD_WITH_GOOGLE_FRP := true
+BUILD_WITH_GOOGLE_GMS_EXPRESS := false
+
+# Add widevine L3 support
+BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 3
+
+# camera enable
+BOARD_CAMERA_SUPPORT := true
+ALLOW_MISSING_DEPENDENCIES=true
+
+# Config GO Optimization
+BUILD_WITH_GO_OPT := true
+
+#Config omx to support codec type.
+BOARD_SUPPORT_VP9 := true
+BOARD_SUPPORT_VP6 := false
+BOARD_SUPPORT_HEVC_ENC := true
+
+# Allow deprecated BUILD_ module types to get DDK building
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_USES_BUILD_HOST_EXECUTABLE := true
+BUILD_BROKEN_USES_BUILD_HOST_SHARED_LIBRARY := true
+BUILD_BROKEN_USES_BUILD_HOST_STATIC_LIBRARY := true
+
+BOARD_SHOW_HDMI_SETTING ?= true
+
+# for dynamaic afbc target 
+BOARD_HS_DYNAMIC_AFBC_TARGET := false
+
+PRODUCT_HAVE_OPTEE := true
+
+#trust is merging into uboot
+BOARD_ROCKCHIP_TRUST_MERGE_TO_UBOOT := true
+
+BOARD_BASEPARAMETER_SUPPORT := true
+
+TARGET_SUPPORTS_OMX_SERVICE := false
