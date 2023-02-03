81ca12c7bbbb310519e4705b2f1c231f5cf373b3
Bian Jin chen <kenjc.bian@rock-chips.com>
2023-02-03 11:04:50 +0800

    Separate the rknn related files from the device.
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I522744c9118e86838d3363e663a54ed673679c8f

--- device.mk
+++ device.mk
@@ -18,17 +18,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_partial_updates=false
 
-BOARD_SEPOLICY_DIRS += vendor/rockchip/hardware/interfaces/neuralnetworks/1.0/default/sepolicy
-PRODUCT_PACKAGES += \
-    public.libraries-rockchip \
-    librknn_api_android \
-    rknn_server \
-    librknnhal_bridge.rockchip \
-    rockchip.hardware.neuralnetworks@1.0-impl \
-    rockchip.hardware.neuralnetworks@1.0-service
-
-$(call inherit-product-if-exists, vendor/rockchip/common/npu/npu.mk)
-
 TARGET_SYSTEM_PROP += device/rockchip/rk3562/rk3562.prop
 # enable this for support f2fs with data partion
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
