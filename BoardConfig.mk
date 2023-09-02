1edd120c8967f81a8d144ce660c31594aae75181
Wu Liangqing <wlq@rock-chips.com>
2023-09-02 07:55:56 +0000

    removed PRODUCT_KERNEL_VERSION
    Change-Id: I78557bc5e79f6296c13a9b87b114c309d01d5a88

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,8 +50,6 @@ BOARD_OVERRIDE_RS_CPU_VARIANT_64 := cortex-a53
 TARGET_USES_64_BIT_BCMDHD := true
 TARGET_USES_64_BIT_BINDER := true
 
-PRODUCT_KERNEL_VERSION := 5.10
-
 # HACK: Build apps as 64b for volantis_64_only
 ifneq (,$(filter ro.zygote=zygote64, $(PRODUCT_DEFAULT_PROPERTY_OVERRIDES)))
 TARGET_PREFER_32_BIT_APPS :=
